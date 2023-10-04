5a1c8917eb4132c663e8119d14e6cb4107488339
Zachary Stone <zach.stone.developer@gmail.com>
2023-10-03 20:13:56 -0400

    update config

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,30 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/zach-portfolio/';
+const base = '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	vitePlugin: {
-		inspector: {
-			showToggleButton: 'always'
-		}
-	},
 
 	kit: {
 		adapter: adapter({ fallback: '404.html' }),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? base : ''
 		}
+	},
+
+	vite: {
+		// Vite-specific configuration goes here
+		plugins: [
+			{
+				inspector: {
+					showToggleButton: 'always'
+				}
+			}
+		]
 	}
 };
 
