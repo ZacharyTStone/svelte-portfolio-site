bf7e0118a329baf9c466fede8668f3f0f95ee339
Zach Stone <zach.stone.developer@gmail.com>
2023-10-06 21:29:09 -0400

    test vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
 	import '$lib/index.scss';
 	import { onHydrated, theme } from '$lib/stores/theme';
 	import { onMount } from 'svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 
 	export const prerender = true;
 
