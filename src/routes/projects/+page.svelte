a7608205f5c0ff4e4a9888bf19f4fc0fdbdd48d3
ZacharyTStone <Zach.Stone.Developer@gmail.com>
2023-11-10 18:42:13 -0500

    update feature project heading to no add padding or margin to featured project cards

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -97,11 +97,7 @@
 			<p class="font-300">Could not find anything...</p>
 		</div>
 	{:else}
-		<h4
-			class="text-[var(--accent-text)] text-1.5em font-500 m-y-12 underline flex justify-center items-center"
-		>
-			Featured Projects
-		</h4>
+		<h4 class="text-[var(--accent-text)] text-1.5em font-300 m-y-12 mx-auto">Featured Projects</h4>
 		<div class="projects-list mt-5 mx-auto">
 			{#each displayed as project}
 				{#if project.featured}
