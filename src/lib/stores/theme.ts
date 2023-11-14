7a44e662d7c775a76565f717f02532bc89b8766f
ZacharyTStone <Zach.Stone.Developer@gmail.com>
2023-11-13 21:54:32 -0500

    refactor and set theme in html to fix flash of white

--- src/lib/stores/theme.ts
+++ src/lib/stores/theme.ts
@@ -1,47 +1,28 @@
-import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
-import { locale } from 'svelte-i18n';
+import { browser } from '$app/environment';
 
-interface ThemeStore extends Writable<string> {}
-
+// Define constants and utility functions
 const STORAGE_KEY = 'zach-stone-portfolio-theme';
+const prefersDarkMode = () => browser && window.matchMedia('(prefers-color-scheme: dark)').matches;
+const updateLocalStorage = (value: string) => browser && localStorage.setItem(STORAGE_KEY, value);
 
-const updateLocalStorage = (value: string): void => {
-	if (browser) {
-		localStorage.setItem(STORAGE_KEY, value);
-	}
-};
-
-// Check system preference for dark mode
-const prefersDarkMode: boolean =
-	browser && window.matchMedia('(prefers-color-scheme: dark)').matches;
-
+// Define the theme store
+interface ThemeStore extends Writable<string> {}
 export const theme: ThemeStore = writable('light');
 
-export const toggleTheme = (newThemeChoice: 'light' | 'dark'): void => {
-	theme.update((newValue) => {
-		console.log('newThemeChoice', newThemeChoice);
-		newValue = newThemeChoice;
-
-		updateLocalStorage(newThemeChoice);
-
-		if (browser) {
-			const rootElement = document.querySelector(':root');
-			rootElement?.setAttribute('data-theme', newValue);
-		}
-
-		return newValue;
-	});
+// Define functions to manage theme
+export const toggleTheme = (newTheme: 'light' | 'dark') => {
+	theme.set(newTheme);
+	updateLocalStorage(newTheme);
+	if (browser) {
+		const rootElement = document.querySelector(':root');
+		rootElement?.setAttribute('data-theme', newTheme);
+	}
 };
 
-export const onHydrated = (): void => {
-	let preferredTheme = localStorage.getItem(STORAGE_KEY) as 'light' | 'dark' | null;
-
-	if (preferredTheme === null) {
-		// If nothing is in local storage, use system preference
-		preferredTheme = prefersDarkMode.toString() ? 'dark' : 'light';
-	}
-
+export const onHydrated = () => {
+	const storedTheme = localStorage.getItem(STORAGE_KEY) as 'light' | 'dark' | null;
+	const preferredTheme = storedTheme ?? (prefersDarkMode() ? 'dark' : 'light');
 	toggleTheme(preferredTheme);
 };
