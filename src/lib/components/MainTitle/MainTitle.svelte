84c4b53cf0b53809357c765af411ca1b92b7e101
ZacharyTStone <Zach.Stone.Developer@gmail.com>
2024-02-09 23:13:01 -0500

    more mobile text size changes

--- src/lib/components/MainTitle/MainTitle.svelte
+++ src/lib/components/MainTitle/MainTitle.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <h1
-	class={`font-[var(--title-f)] font-black tracking-[4px] text-center text-2.5em ${classes}`}
+	class={`font-bold text-center sm:text-5xl text-3xl ${classes}`}
 	style="background: linear-gradient(var(--main-text), var(--accent-text-hover)); -webkit-background-clip: text; background-clip: text; -webkit-text-fill-color: transparent; word-wrap: break-word; white-space: normal;"
 >
 	<slot />
