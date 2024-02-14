79e2bca225071b33ffecdacd83de92a7662a0556
ZacharyTStone <Zach.Stone.Developer@gmail.com>
2024-02-14 16:43:33 -0500

    remove resume only page

--- src/lib/components/NavMenu/NavMenu.svelte
+++ src/lib/components/NavMenu/NavMenu.svelte
@@ -31,8 +31,8 @@
 		{ title: NavBar.about, to: '/about', icon: 'i-carbon-user' },
 		{ title: NavBar.skills, to: '/skills', icon: 'i-carbon-software-resource-cluster' },
 		{ title: NavBar.personal, to: '/projects', icon: 'i-carbon-cube' },
-		{ title: NavBar.career, to: '/experience', icon: 'i-carbon-development' },
-		{ title: NavBar.resume, to: '/resume', icon: 'i-carbon-result' }
+		{ title: NavBar.career, to: '/experience', icon: 'i-carbon-development' }
+		// { title: NavBar.resume, to: '/resume', icon: 'i-carbon-result' }
 	];
 </script>
 
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,7 +5,9 @@
 	import { getPlatfromIcon } from '$lib/params';
 	import { _ } from 'svelte-i18n';
 
-	import { HOME } from '$lib/params';
+	import Card from '$lib/components/Card/Card.svelte';
+
+	import { HOME, RESUME } from '$lib/params';
 
 	let { description, lastName, links, name, title, skills } = HOME;
 </script>
@@ -23,12 +25,19 @@
 			<h2 class="text-xl md:text-2xl font-semibold mb-2">{$_('ABOUT.quote2.title')}</h2>
 			<p class="mb-6">{$_('ABOUT.quote2.text')}</p>
 			<hr class="border-t border-gray-300 mb-6" />
-			<div class="flex flex-wrap gap-3">
-				{#each links as { platform, link }}
-					<ChipIcon name={platform} href={link} newtab>
-						<Icon icon={getPlatfromIcon(platform)} color={'var(--accent-text)'} size={'28px'} />
-					</ChipIcon>
-				{/each}
+			<div class="flex flex-wrap justify-center align-center gap-4">
+				<div class="flex gap-3">
+					{#each links as { platform, link }}
+						<ChipIcon name={platform} href={link} newtab>
+							<Icon icon={getPlatfromIcon(platform)} color={'var(--accent-text)'} size={'28px'} />
+						</ChipIcon>
+					{/each}
+				</div>
+				<div class="flex gap-3">
+					{#each RESUME?.links as item}
+						<Card href={item?.to}>{$_(item?.label)}</Card>
+					{/each}
+				</div>
 			</div>
 		</main>
 	</div>
