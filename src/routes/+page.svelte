d0f7ebb09585cb5613c3fc5441702970657a1f7a
ZacharyTStone <Zach.Stone.Developer@gmail.com>
2024-03-24 09:41:53 -0400

    test particles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-	import { onMount, onDestroy } from 'svelte';
-	import { writable } from 'svelte/store'; // Importing writable store
-	import { fade } from 'svelte/transition'; // Importing fade transition
-	import { quintInOut } from 'svelte/easing'; // Importing quintInOut easing
+	import { onMount } from 'svelte';
+	import { loadFull } from 'tsparticles';
+	import { writable } from 'svelte/store';
 	import Carrousel from '$lib/components/Carrousel/Carrousel.svelte';
 	import ChipIcon from '$lib/components/Chip/ChipIcon.svelte';
 	import Icon from '$lib/components/Icon/Icon.svelte';
@@ -13,6 +12,193 @@
 	import { _ } from 'svelte-i18n';
 	import { base } from '$app/paths';
 	import { getAssetURL } from '$lib/data/assets';
+	import Computer from '$lib/assets/images/computer.svg';
+	import { quintInOut } from 'svelte/easing';
+	import { fade } from 'svelte/transition';
+	// Particles
+
+	let particlesInit = async (main: any) => {
+		await loadFull(main);
+	};
+
+	let particlesLoaded = (container: any) => {
+		console.log(container);
+	};
+
+	let Particles: any = null;
+
+	let particlesConfig: any = {
+		detectRetina: false,
+		fpsLimit: 60,
+		interactivity: {
+			detectsOn: 'canvas',
+			events: {
+				onClick: {
+					enable: false,
+					mode: 'push'
+				},
+				onDiv: {
+					elementId: 'repulse-div',
+					enable: false,
+					mode: 'repulse'
+				},
+				onHover: {
+					enable: true,
+					mode: 'bubble',
+					parallax: {
+						enable: false,
+						force: 2,
+						smooth: 10
+					}
+				},
+				resize: true
+			},
+			modes: {
+				bubble: {
+					distance: 40,
+					duration: 2,
+					opacity: 8,
+					size: 6,
+					speed: 3
+				},
+				connect: {
+					distance: 80,
+					lineLinked: { opacity: 0.5 },
+					radius: 60
+				},
+				grab: {
+					distance: 400,
+					lineLinked: { opacity: 1 }
+				},
+				push: { quantity: 4 },
+				remove: { quantity: 2 },
+				repulse: {
+					distance: 200,
+					duration: 0.4
+				},
+				slow: {
+					active: false,
+					radius: 0,
+					factor: 1
+				}
+			}
+		},
+		particles: {
+			color: { value: '#ffffff' },
+			lineLinked: {
+				blink: false,
+				color: '#ffffff',
+				consent: false,
+				distance: 30,
+				enable: true,
+				opacity: 0.4,
+				width: 1
+			},
+			move: {
+				attract: {
+					enable: false,
+					rotate: {
+						x: 600,
+						y: 1200
+					}
+				},
+				bounce: false,
+				direction: 'none',
+				enable: true,
+				outMode: 'bounce',
+				random: false,
+				speed: 0.5,
+				straight: false
+			},
+			number: {
+				density: {
+					enable: false,
+					area: 2000
+				},
+				limit: 0,
+				value: 200
+			},
+			opacity: {
+				animation: {
+					enable: true,
+					minimumValue: 0.05,
+					speed: 2,
+					sync: false
+				},
+				random: false,
+				value: 0.4
+			},
+			shape: {
+				character: {
+					fill: false,
+					font: 'Verdana',
+					style: '',
+					value: '*',
+					weight: '400'
+				},
+
+				polygon: { sides: 5 },
+				stroke: {
+					color: '#000000',
+					width: 0
+				},
+				type: 'circle'
+			},
+			size: {
+				animation: {
+					enable: false,
+					minimumValue: 0.1,
+					speed: 40,
+					sync: false
+				},
+				random: true,
+				value: 3
+			}
+		},
+		polygon: {
+			draw: {
+				enable: true,
+				stroke: {
+					color: '#ffffff',
+					width: 0.5,
+					opacity: 0.35
+				}
+			},
+			enable: true,
+			move: {
+				radius: 10
+			},
+			inlineArrangement: 'equidistant',
+			scale: 1,
+			type: 'inline',
+
+			polygons: {
+				enable: true,
+				scale: 0.5,
+				type: 'inline',
+				move: {
+					radius: 10
+				},
+				url: Computer,
+				inlineArrangement: 'equidistant',
+				draw: {
+					enable: true,
+					stroke: {
+						color: 'red',
+						width: 0.5,
+						opacity: 0.35
+					}
+				}
+			},
+			data: {
+				size: {
+					width: 515.9,
+					height: 728.5
+				}
+			}
+		},
+		retina_detect: false
+	};
 
 	let { description, lastName, links, name, title, skills } = HOME;
 
@@ -21,6 +207,12 @@
 		document.title = useTitle(title, TITLE_SUFFIX);
 		const interval = setInterval(cycleSkills, 10000); // Cycle every 10 seconds
 		return () => clearInterval(interval);
+	});
+
+	onMount(async () => {
+		// @ts-ignore
+		const module = await import('svelte-particles');
+		Particles = module.default;
 	});
 
 	// Store to hold the index of the currently displayed skill
@@ -75,6 +267,16 @@
 			{/key}
 		{/if}
 	</div>
+	{#if Particles}
+		<div id="tsparticles" class="tsparticles-container">
+			<Particles
+				id="tsparticles"
+				init={particlesInit}
+				loaded={particlesLoaded}
+				options={particlesConfig}
+			/>
+		</div>
+	{/if}
 </div>
 
 <style>
@@ -85,5 +287,16 @@
 		min-height: 100vh;
 		display: flex;
 		flex-direction: column;
+	}
+
+	:global(#tsparticles) {
+		margin: 0;
+		padding: 0;
+		position: fixed;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+		z-index: 0;
 	}
 </style>
