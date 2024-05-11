8fec010531eebf4935bb6f5250f61c060574ae80
ZacharyTStone <Zach.Stone.Developer@gmail.com>
2024-05-11 05:25:11 -0400

    update main title to have overflow vissible to fix clipping

--- src/lib/components/MainTitle/MainTitle.svelte
+++ src/lib/components/MainTitle/MainTitle.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <h1
-	class={`font-bold text-center sm:text-5xl text-3xl ${classes}`}
+	class={`font-bold text-center sm:text-5xl text-3xl overflow-visible ${classes}`}
 	style="background: linear-gradient(var(--main-text), var(--accent-text-hover)); -webkit-background-clip: text; background-clip: text; -webkit-text-fill-color: transparent; word-wrap: break-word; white-space: normal;"
 >
 	<slot />
