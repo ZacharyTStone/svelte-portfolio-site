2fe53df158a499cf1927f55694477e55493e2b94
ZacharyTStone <Zach.Stone.Developer@gmail.com>
2024-08-04 08:35:54 +0900

    test: try to fix prerendering build issue

--- src/routes/experience/[slug]/+page.ts
+++ src/routes/experience/[slug]/+page.ts
@@ -1,5 +1,13 @@
 import MY_EXPERIENCES from '$lib/experiences.params';
 
+export const prerender = true;
+
+export function entries() {
+	return MY_EXPERIENCES.map((experience) => ({
+		slug: experience.slug
+	}));
+}
+
 export function load({ params }: { params: Record<string, string> }) {
 	if (params.slug) {
 		const experience = MY_EXPERIENCES.find((item) => {
--- src/routes/projects/[slug]/+page.ts
+++ src/routes/projects/[slug]/+page.ts
@@ -1,5 +1,13 @@
 import MY_PROJECTS from '$lib/projects.params';
 
+export const prerender = true;
+
+export function entries() {
+	return MY_PROJECTS.map((project) => ({
+		slug: project.slug
+	}));
+}
+
 export function load({ params }: { params: Record<string, string> }) {
 	if (params.slug) {
 		const project = MY_PROJECTS.find((item) => {
