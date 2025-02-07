3b465cffa571e4307dfd87f67caa9f8af34045e1
ZacharyTStone <Zach.Stone.Developer@gmail.com>
2025-02-07 18:20:32 +0900

    fix overflow cut off for h1

--- src/lib/components/MainTitle/MainTitle.svelte
+++ src/lib/components/MainTitle/MainTitle.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <h1
-	class={`font-bold text-center sm:text-5xl text-3xl overflow-visible ${classes}`}
+	class={`pb-2 font-bold text-center sm:text-5xl text-3xl overflow-visible ${classes}`}
 	style="background: linear-gradient(var(--main-text), var(--accent-text-hover)); -webkit-background-clip: text; background-clip: text; -webkit-text-fill-color: transparent; word-wrap: break-word; white-space: normal;"
 >
 	{@render children?.()}
